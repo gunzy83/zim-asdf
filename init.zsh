55eed0cb079247bd1172b74c5916ad3693844b8c
Ross Williams <gunzy83au@gmail.com>
2024-03-22 21:00:08 +1000

    Remove auto-install and ignore if the asdf.sh does not exist (host machine for my setups with distrobox)

--- init.zsh
+++ init.zsh
@@ -9,24 +9,20 @@ if (( ! ${+ASDF_DIR} )); then
 fi
 export ASDF_DIR
 
-# Install using git if not already installed at ASDF_DIR
-if [[ ! -e ${ASDF_DIR} ]]; then
-  print -u2 -R "${ASDF_DIR} not found, installing asdf"
-  command git clone -q https://github.com/asdf-vm/asdf.git ${ASDF_DIR} || return 1
-  command git -C ${ASDF_DIR} checkout -q $(command git -C ${ASDF_DIR} describe --abbrev=0 --tags) -- || return 1
+if [[ -e ${ASDF_DIR}/asdf.sh ]]; then
+  path=(${ASDF_DIR}/bin ${path:#${ASDF_DIR}/bin})
+  fpath+=(${ASDF_DIR}/completions(FN))
+
+  # Don't add shims directory to the path if direnv plugin is installed
+  local asdf_data=${ASDF_DATA_DIR:-${HOME}/.asdf}
+  path=(${asdf_data}/shims ${path:#${asdf_data}/shims})
+  unset asdf_data
+
+  source ${ASDF_DIR}/asdf.sh
+
+  # java home support
+  if [[ -e ${ASDF_DIR}/plugins/java/set-java-home.zsh ]]; then
+    source ${ASDF_DIR}/plugins/java/set-java-home.zsh
+  fi
 fi
 
-path=(${ASDF_DIR}/bin ${path:#${ASDF_DIR}/bin})
-fpath+=(${ASDF_DIR}/completions(FN))
-
-# Don't add shims directory to the path if direnv plugin is installed
-local asdf_data=${ASDF_DATA_DIR:-${HOME}/.asdf}
-path=(${asdf_data}/shims ${path:#${asdf_data}/shims})
-unset asdf_data
-
-source ${ASDF_DIR}/asdf.sh
-
-# java home support
-if [[ -e ${ASDF_DIR}/plugins/java/set-java-home.zsh ]]; then
-  source ${ASDF_DIR}/plugins/java/set-java-home.zsh
-fi	
